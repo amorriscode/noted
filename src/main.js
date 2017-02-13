374b27f9a2586e45b1fb51f29c265cbd22862348
Anthony M <anthony@amorrissound.com>
2017-02-13 15:26:46 -0800

    Factor router into it's own file

--- src/main.js
+++ src/main.js
@@ -1,50 +1,11 @@
 // Setup the app
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import VueFire from 'vuefire';
 import App from './App.vue';
+import store from './store';
+import router from './router';
 
 Vue.use(VueFire);
-Vue.use(VueRouter);
-
-// Import store
-import store from './store';
-
-// Setup routes
-import Login from './components/Login.vue';
-import Signup from './components/Signup.vue';
-import Notes from './components/Notes.vue';
-
-const routes = [
-  { path: '/login', component: Login },
-  { path: '/signup', component: Signup },
-  { path: '/notes', component: Notes }
-]
-
-const router = new VueRouter({
-  routes
-})
-
-// Go to the login page if user isn't logged in
-router.beforeEach((to, from, next) => {
-  // Make the user login persist
-  const notedUser = JSON.parse(localStorage.getItem('noted_user'));
-  if (notedUser && !store.state.user) {
-    store.commit('setUser', notedUser);
-  }
-
-  if (store.state.user) return next();
-
-  // Allow users to signup
-  if (to.path === '/signup') return next();
-
-  // Don't change route if it's already set to /login
-  if (to.path !== '/login') {
-    next('/login');
-  } else {
-    next();
-  }
-});
 
 const vm = new Vue({
   router,
