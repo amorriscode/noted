1ac8e09136a75a118cf33994b79d078d75d916f5
Anthony M <anthony@amorrissound.com>
2017-02-12 17:01:02 -0800

    Setup App.vue so I can use vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
+// Setup the app
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import VueFire from 'vuefire';
 import App from './App.vue';
 
 Vue.use(VueFire);
+Vue.use(VueRouter);
+
+// Setup routes
+import Login from './components/Login.vue';
+
+const routes = [
+  { path: '/login', component: Login }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 const vm = new Vue({
+  router,
   el: 'app',
   components: { App }
 });
